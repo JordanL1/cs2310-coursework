faea29c37f5dc882081dd8247eae9c0e1007cdb1
Jordan Lees <leesja@aston.ac.uk>
2018-12-11 20:07:34 +0000

    Refactored Importer: imports on construction, getters used to access data after construction. Single private method used to import groups and dances. Tested and working.

--- src/data/Importer.java
+++ src/data/Importer.java
@@ -12,7 +12,8 @@ import java.io.FileNotFoundException;
 
 /**
  * The Importer class is used to read relevant data from CSV files defined as static constants.
- * Methods exist for returning collections of data in a usable format.
+ * Methods exist for returning collections of data in a usable format. Data is imported when an
+ * instance is constructed and can then be retrieved using getters.
  * 
  * @author Jordan Lees
  * @version 10/12/2018
@@ -24,29 +25,48 @@ public class Importer {
 	private static final String DANCE_GROUPS = "data/danceShowData_danceGroups.csv";
 	private static final String RUNNING_ORDER = "data/danceShowData_runningOrder.csv";
 	
+	LinkedHashMap<String, Set<String>> groups;
+	LinkedHashMap<String, Set<String>> dances;
+	
 	public Importer() {
-		
+		try {
+			groups = makeData(DANCE_GROUPS);
+			dances = makeData(DANCES);
+		}
+		catch (IOException e) {
+			System.out.println("Could not import data: " + e.getMessage());
+		}
+	}
+	
+	public LinkedHashMap<String, Set<String>> getDanceGroups() {
+		return groups;
+	}
+	
+	public LinkedHashMap<String, Set<String>> getDances() {
+		return dances;
 	}
 	
 	/**
-	 * Read the CSV file defined as a static constant and produce a data structure containing
-	 * dance group names and their associated dancers.
+	 * Read the chosen CSV file and produce a map where values are sets.
+	 * CSV must be formatted as two columns, where col1 is the key and col2 contains
+	 * comma-separated values which will be contained in the set.
 	 * 
+	 * @param file Filename of CSV to import from
 	 * @return Map of dance group names (key) with their set of performers (value).
 	 * @throws IOException
-	 */
-	public LinkedHashMap<String, Set<String>> getDanceGroups() throws IOException {
+	 */	
+	private LinkedHashMap<String, Set<String>> makeData(String file) throws IOException {
 		BufferedReader in;
 		Scanner scan;
-		LinkedHashMap<String, Set<String>> danceGroups = new LinkedHashMap<String, Set<String>>();
+		LinkedHashMap<String, Set<String>> data = new LinkedHashMap<String, Set<String>>();
 		
-		in = new BufferedReader(new FileReader(DANCE_GROUPS));
+		in = new BufferedReader(new FileReader(file));
 		in.readLine();
 
 		while(in.ready()) {
 			String key;
 			LinkedHashSet<String> set = new LinkedHashSet<String>();
-			scan = new Scanner(in.readLine()).useDelimiter(", |\\t");
+			scan = new Scanner(in.readLine()).useDelimiter(", |\\t\\s*");
 			
 			key = scan.next();
 			
@@ -55,48 +75,12 @@ public class Importer {
 			}
 			
 			scan.close();
-			danceGroups.put(key, set);
+			data.put(key, set);
 		}
 		
 		in.close();
 		
-		return danceGroups;
-	}
-	
-	/**
-	 * Read the CSV file defined as a static constant and produce a data structure containing
-	 * dance titles and their associated performers (which may be either performer names or names
-	 * of dance groups).
-	 * 
-	 * @return Map of dance titles (key) with their set of performers (value).
-	 * @throws IOException
-	 */
-	public LinkedHashMap<String, Set<String>> getDances() throws IOException {
-		BufferedReader in;
-		Scanner scan;
-		LinkedHashMap<String, Set<String>> dances = new LinkedHashMap<String, Set<String>>();
-		
-		in = new BufferedReader(new FileReader(DANCES));
-		in.readLine();
-
-		while(in.ready()) {
-			String key;
-			LinkedHashSet<String> set = new LinkedHashSet<String>();
-			scan = new Scanner(in.readLine()).useDelimiter(", |\\t");
-			
-			key = scan.next();
-			
-			while(scan.hasNext()) {
-				set.add(scan.next());
-			}
-			
-			scan.close();
-			dances.put(key, set);
-		}
-		
-		in.close();
-		
-		return dances;
+		return data;
 	}
 	
 }
